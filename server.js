d68e8edb114fb71bbc5665f91ca5b78aa0cde9ad
carlos apablaza <c.apablaza.z@gmail.com>
2018-05-12 22:16:30 -0300

    rename environment directory

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 var app = require('./app/app'); 
 
-const enviroment = require('./config/enviroments/factoryEnv');
+const environment = require('./config/environment/factoryEnv');
 
 const apiPort = process.env.PORT || 3000;
 
 app.listen(apiPort, () => {
-    console.log('using enviroment : ');
-    console.log(JSON.stringify(enviroment.getDatabase()));    
-    console.log(JSON.stringify(enviroment.getMongoDB()));
-    console.log(JSON.stringify(enviroment.getApiRemote()));
+    console.log('using environment : ');
+    console.log(JSON.stringify(environment.getDatabase()));    
+    console.log(JSON.stringify(environment.getMongoDB()));
+    console.log(JSON.stringify(environment.getApiRemote()));
 });
